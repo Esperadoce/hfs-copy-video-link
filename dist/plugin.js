375b37c8e1c98ebf26b8bbb3e0c546475a5a5b1d
Hicham Bouchikhi <hbouchikhi@outlook.com>
2025-12-27 19:22:18 +0100

    Update repository name in plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,4 +1,4 @@
 exports.version = 0.2;
 exports.description = "Adds a button to copy direct video links to clipboard";
-exports.repo = "Esperadoce/hfs-plugin-copy-video-link";
+exports.repo = "Esperadoce/hfs-copy-video-link";
 exports.frontend_js = ["main.js"];
